094ae38e87fa877a08efed608f5fd37d618702ef
Ahmedmo1910 <2202105@student.eelu.edu.eg>
2025-11-14 01:52:26 +0200

    Add CoursesScreenBody widget with video player functionality and update dependencies

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color primaryColor = Color(0xFF022D4F);
   static const Color darkPrimaryColor = Color(0xFF091B29);
+  static const Color handleColor =Color(0xFF58AF95);
 }
