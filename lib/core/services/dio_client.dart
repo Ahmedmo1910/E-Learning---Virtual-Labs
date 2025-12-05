056fe6f57963f22c53f93f1bf91ee9f45b78d83d
Ahmedmo1910 <2202105@student.eelu.edu.eg>
2025-12-06 01:27:09 +0200

    Implement secure storage for authentication tokens and refactor related components

--- lib/core/services/dio_client.dart
+++ lib/core/services/dio_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
-import 'package:e_learning/core/services/api_pref_helper.dart';
+import 'package:e_learning/core/helper_functions/secure_storage.dart';
+import 'package:e_learning/features/auth/data/auth_repo.dart';
 
 class DioClient {
   final Dio _dio = Dio(
@@ -13,18 +14,35 @@ class DioClient {
       receiveTimeout: Duration(seconds: 10),
     ),
   );
+
   DioClient() {
     _dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) async {
-          final token = await ApiPrefHelper.getToken();
+          final token = await SecureStorage.getAccessToken();
           if (token != null && token.isNotEmpty) {
             options.headers['Authorization'] = 'Bearer $token';
           }
           return handler.next(options);
         },
+
+        onError: (error, handler) async {
+          if (error.response?.statusCode == 401) {
+            final refreshed = await AuthRepo().refreshToken(); // boolean
+            if (refreshed) {
+              final newToken = await SecureStorage.getAccessToken();
+              error.requestOptions.headers["Authorization"] =
+                  "Bearer $newToken";
+
+              final cloneReq = await _dio.fetch(error.requestOptions);
+              return handler.resolve(cloneReq);
+            }
+          }
+          return handler.next(error);
+        },
       ),
     );
   }
+
   Dio get dio => _dio;
 }
